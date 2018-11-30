4f8283879906207c70a4fa317c679a300f974a35
Kush Patel <kushpatel@asite.com>
2018-11-30 10:02:19 +0530

    Network call video 34

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios' ;
 
 
 class AlbumList extends Component {
 
   componentWillMount() {
-    console.log('component will mount in albu lis');
+    console.log('component will mount in album list');
+    //debugger;
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
+     // fetch ('https://rallycoding.herokuapp.com/api/music_albums')
+     // .then(response => console.log(response));
   }
 
 render() {
