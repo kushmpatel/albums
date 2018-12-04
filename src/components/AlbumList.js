e2a91014d73d39f34db633bc9d69bf60b6719d7c
Kush Patel <kushpatel@asite.com>
2018-12-04 16:47:32 +0530

    Handled response

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import axios from 'axios' ;
+import axios from 'axios';
 
 
 class AlbumList extends Component {
+
+  state = { albums: [] } ;
 
   componentWillMount() {
     console.log('component will mount in album list');
     //debugger;
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response));
+    .then(response => this.setState({ albums: response.data }));
      // fetch ('https://rallycoding.herokuapp.com/api/music_albums')
      // .then(response => console.log(response));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+    <Text key={album.title}>{album.title}</Text>);
   }
 
 render() {
    return (
       <View>
-        <Text>AlbumList !!!1 </Text>
+        {this.renderAlbums()}
 
      </View>
   );
